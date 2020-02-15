c2415a08e2ae3f00413eff07215a4f5908bfacd9
dutie <r0695565@student.thomasmore.be>
2020-02-15 20:55:37 +0100

    Finished qr-generator
    upload to server! (app.js = main file)

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
+var ObjectID = require('mongodb').ObjectID;
 
 const db = require("./db");
 const collection = "Device";
 const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
 // parses json data sent to us by the user 
 app.use(bodyParser.json());
 
@@ -25,8 +30,16 @@ app.get('/getDevices', (req, res) => {
     });
 });
 
-app.get('/getTypes', (req, res) => {
+//delete
 
+
+app.get('/getTypes', (req, res) => {
+    db.getDB().collection(collection).deleteMany({
+        'name': ""
+    },
+    {
+        'type':"existing types"
+    });
     db.getDB().collection('Device').distinct("type", {}, (function (err, documents) {
         if (err)
             console.log(err);
@@ -37,24 +50,40 @@ app.get('/getTypes', (req, res) => {
         }
     }));
 });
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'qr.html'));
+})
+app.get('/getJson', (req, res) => {
+    console.log("get json now");
+    db.getDB().collection(collection).find().sort({
+        '_id': -1
+    }).limit(1).forEach(
+        function (doc) {
+            console.log(doc._id);
+            res.json(doc._id);
+        });
+});
 
 //create
 app.post('/', (req, res, next) => {
     // Document to be inserted
     const userInput = req.body;
-
     db.getDB().collection(collection).insertOne(userInput, (err, result) => {
         if (err) {
             const error = new Error("Failed to insert device Document");
             error.status = 400;
             next(error);
-        } else
+        } else {
+
             res.json({
                 result: result,
                 document: result.ops[0],
                 msg: "Successfully inserted device!!!",
                 error: null
             });
+
+        }
+
     });
     db.getDB().collection(collection).find().sort({
         '_id': -1
